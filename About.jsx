1584fe03e736d22ea9262d9e547a47306f25aed6
RamKoushik05 <94036997+RamKoushik05@users.noreply.github.com>
2021-11-10 12:12:18 +0530

    Create About.jsx

---
+++ About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+
+const About =() => {
+   return (
+     <><div>
+     <h1 className='text-center'>About us</h1>
+   <center>  <h5>We are the leading in real estate management sectors.
+     
+
+
+
+
+Our customers place a lot of trust when they choose us as a partner for fulfillment of their dreams - be it buying a dream home or investing their hard earned money. our endeavor is to become a preferred real estate services brand of choice for all our customers’ needs across their life cycle - a brand that customers will not only just trust but also happily endorse. Keeping this customer insight in mind, we have created a unique strategy & structure to present our spectrum of businesses and offerings under one virtual brand. From a customer perspective, this offers simplicity & convenience. For our employees, we offer a world of growth opportunities across all our financial services offerings. </h5> </center>
+     </div>
+ </>  
+  )
+}
+
+export default About;
