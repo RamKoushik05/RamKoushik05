fb00c1907f4b06e1e479127c758a3c4063c91b3c
RamKoushik05 <94036997+RamKoushik05@users.noreply.github.com>
2021-11-10 12:11:55 +0530

    Create Services.jsx

---
+++ Services.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Common from './Common';
+
+
+
+const Service =() => {
+   return (
+     <> <div>
+    <center> <div className="my-5">
+       <h1 className='text-left'>Our Services</h1>
+       </div>
+       <div className="container-fulid nav-bg">
+       <div className="row">
+        <div className="col-20 mux-auto">
+          <div className='row'>
+            <div className='col-md-5 mx-auto'>
+            <div class="card">
+
+  <div class="card-body">
+    <h5 class="card-title">REAL ESTATE</h5>
+    <p class="card-text">We are we are providing a wide variety of Real Estate properties to our customers which will be varies according to the need of customer</p>
+    <a href="#" class="btn btn-primary">See Plans</a>
+  </div>
+
+</div>
+
+<div class="card">
+
+  
+</div> 
+            </div>
+          </div>
+        </div>
+        </div>
+        </div></center>  
+        </div>
+     </>
+   )
+}
+
+export default Service;
