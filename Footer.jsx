95a13f92d6c7552bd77d7f959caf079391434954
RamKoushik05 <94036997+RamKoushik05@users.noreply.github.com>
2021-11-10 12:16:15 +0530

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,17 @@
+const Footer=()=>{
+    return(
+        <>
+        <footer className= "bg-light text-center">
+        <div className="">
+            <p><div>
+                REAL ESTATE MANAGEMENT: REM-433
+              <br /> 
+              This webiste is made according to project given by K L university.
+           </div> </p>
+</div>
+        </footer>
+       
+        </>
+    )
+}
+export default Footer;
